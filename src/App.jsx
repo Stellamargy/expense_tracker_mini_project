bce2a43faaf329a74a1a119da1548f6949c4ff55
stlmargy <stellahmargy0211@gmail.com>
2025-04-21 16:57:23 +0300

    adds responsive layouts for the app -mobile first

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ import ExpenseForm from "./components/ExpenseForm";
 
 const App = () => {
   return (
-    <div className="app-container ">
-      <header className="app-header">
+    <div className="app-container h-100  d-flex flex-column border border-danger ">
+      <header className="app-header container border border-dark">
         <div className="header-info-wrapper">
           <h1 className="expense-app-name ">Expense Tracker</h1>
           <p className="expense-app-about  ">
@@ -17,18 +17,20 @@ const App = () => {
           </p>
         </div>
       </header>
-      <main className="main-content ">
-        <section className="add-expense-section">
-          <ExpenseForm />
-        </section>
-        <section className="display-expense-section">
-          <div className="search-expense-wrapper">
-            <ExpenseSearch />
-          </div>
-          <div className="expense-list-wrapper">
-            <ExpenseList />
-          </div>
-        </section>
+      <main className="main-content flex-grow-1 container border border-info  ">
+        <div className="row">
+          <section className="add-expense-section col-12 col-md-3 border border-warning">
+            <ExpenseForm />
+          </section>
+          <section className="display-expense-section col-12 col-md-9 border border-warning">
+            <div className="search-expense-wrapper">
+              <ExpenseSearch />
+            </div>
+            <div className="expense-list-wrapper">
+              <ExpenseList />
+            </div>
+          </section>
+        </div>
       </main>
     </div>
   );
