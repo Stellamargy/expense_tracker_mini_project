a69012daa73af67e85c227a769b873904355753e
stlmargy <stellahmargy0211@gmail.com>
2025-04-23 22:34:48 +0300

    sets up local storage and state for expense data

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,33 @@ import ExpenseList from "./components/ExpenseList";
 import ExpenseSearch from "./components/ExpenseSearch";
 import ExpenseForm from "./components/ExpenseForm";
 import Header from "./components/Header";
+import expenseData from "./data/expenseData";
+import { useState } from "react";
+import { useEffect } from "react";
+console.log(expenseData);
 
 const App = () => {
+  const [expenses, setExpenses] = useState([]);
+  // Check localStorage on mount
+  useEffect(() => {
+    const storedData = localStorage.getItem("expenseData");
+    if (!storedData) {
+      localStorage.setItem("expenseData", JSON.stringify(expenseData));
+      setExpenses([...expenseData]);
+    } else {
+      setExpenses([...JSON.parse(storedData)]);
+    }
+  }, []); 
+   // Update localStorage when expenses state changes
+   useEffect(() => {
+    if (expenses.length > 0) {
+      localStorage.setItem("expenseData", JSON.stringify(expenses));
+    }
+  }, [expenses]);
+
   return (
     <div className="app-container h-100  d-flex flex-column  ">
-      < Header />
+      <Header />
       <main className="main-content flex-grow-1 container  ">
         <div className="row">
           <section className="add-expense-section col-12 col-lg-4 mt-4 mt-lg-0">
@@ -22,7 +44,6 @@ const App = () => {
             <div className="expense-list-wrapper mt-4">
               <ExpenseList />
             </div>
-            
           </section>
         </div>
       </main>
--- src/components/ExpenseList.jsx
+++ src/components/ExpenseList.jsx
@@ -3,14 +3,14 @@ import React from "react";
 function ExpenseList() {
   return (
     <div className="mb-4">
-      <div className="table-responsive table-resonsive-large">
+      <div className="table-responsive table-responsive-large">
       <table className="table table-secondary table-hover table-striped">
         <thead>
           <tr>
             <th scope="col">Expense Date</th>
             <th scope="col">Expense Name</th>
             <th scope="col">Expense Description</th>
-            <th scope="col">Expense Amount</th>
+            <th scope="col">Expense Category</th>
             
           </tr>
         </thead>
